7899c8a91f524e3b29e4bed40bb92b1808ffd43e
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-07-07 05:39:45 -0500

    add IMGUIUnstrip as a dependency

--- MSZ-Mod-Menu/Core.cs
+++ MSZ-Mod-Menu/Core.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 [assembly: MelonInfo(typeof(MSZ_Mod_Menu.Core), "MSZ-Mod-Menu", "1.0.0", "gameknight963")]
 [assembly: MelonAuthorColor(255, 86, 65, 157)]
+[assembly: MelonAdditionalDependencies("IMGUIUnstrip")]
 
 namespace MSZ_Mod_Menu
 {
